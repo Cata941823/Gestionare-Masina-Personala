e59be767386174627d107c6e4fb69e539773c4c7
Razvan <cernatrazvanm@yahoo.com>
2020-04-24 22:09:38 +0300

    Am adaugat clasa Documente, functii: afiseaza tot, afiseaza dupa id

---
+++ src/main/java/com/proiectmds/demo/DocumenteService.java
@@ -0,0 +1,29 @@
+package com.proiectmds.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DocumenteService {
+
+    @Autowired
+    DocumenteReposistory documenteReposistory;
+
+    public List<Documente> gasesteDupaIddocument (int iddocument) {
+        return documenteReposistory.findByIddocument(iddocument);
+    }
+
+    public List<Documente> getAllDocumente(){
+        List<Documente> documente = new ArrayList<Documente>();
+        documenteReposistory.findAll().forEach(s -> documente.add(s));
+        return documente;
+    }
+
+
+
+
+
+}
