e2a8b6a76da95b5ede1ebdefc44a257222e93841
PopaAlex19 <popaeduard998@gmail.com>
2020-04-25 00:14:21 +0300

    Rezolvare Problema de compilare

--- src/main/java/com/proiectmds/demo/DocumenteService.java
+++ src/main/java/com/proiectmds/demo/DocumenteService.java
@@ -22,15 +22,5 @@ public class DocumenteService {
         return documente;
     }
 
-<<<<<<< HEAD
-=======
-    public void insertDocument(Documente documente){
-        System.out.println("-------------\n");
-        System.out.println(documente);
-        documenteReposistory.save(documente);
-    }
-    ///testare functie
-
->>>>>>> c0b0966a43a8c8494e7a06e37cc0e86447110911
 
 }
